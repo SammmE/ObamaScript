c8f20e3cc7e25224f9a7f64214d0bd58452aea9f
SammmE <samhithenganti@gmail.com>
2022-12-10 17:12:25 -0500

    added Func class

---
+++ src/Func.js
@@ -0,0 +1,24 @@
+exports.Func = class Func {
+    constructor(name, isJS, method, params) {
+        this.name = name;
+        this.isJS = isJS;
+        this.method = method;
+        this.params = params;
+    }
+
+    getName = () => {
+        return this.name;
+    };
+
+    getParams = () => {
+        return this.params;
+    };
+
+    getMethod = () => {
+        return this.method;
+    };
+
+    getIsJS = () => {
+        return this.isJS;
+    };
+};
